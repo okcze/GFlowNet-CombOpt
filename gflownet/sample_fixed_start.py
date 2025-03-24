42355156a5eb6f309daa0eb2a4a1dfaaa7989967
okcze <piotr.phitz@gmail.com>
2025-03-24 22:06:52 +0100

    feat: fixed starts

---
+++ gflownet/sample_fixed_start.py
@@ -0,0 +1,246 @@
+import sys, os
+import gzip, pickle
+from time import time, sleep
+from tqdm import tqdm
+import hydra
+from omegaconf import DictConfig, open_dict, OmegaConf
+
+import random
+import numpy as np
+import torch
+import dgl
+from einops import rearrange, reduce, repeat
+
+from .data import get_data_loaders
+from .util import seed_torch, TransitionBuffer, get_mdp_class, get_reference_alg
+from .algorithm import DetailedBalanceTransitionBuffer
+
+torch.backends.cudnn.benchmark = True
+
+
+def get_alg_buffer(cfg, device):
+    assert cfg.alg in ["db", "fl"]
+    buffer = TransitionBuffer(cfg.tranbuff_size, cfg)
+    alg = DetailedBalanceTransitionBuffer(cfg, device)
+    return alg, buffer
+
+def get_saved_alg_buffer(cfg, device):
+    """Allow to load model from file."""
+    assert cfg.alg in ["db", "fl"]
+    buffer = TransitionBuffer(cfg.tranbuff_size, cfg)
+    alg = DetailedBalanceTransitionBuffer(cfg, device)
+    alg.load(cfg.alg_load_path)
+    return alg, buffer
+
+def get_logr_scaler(cfg, process_ratio=1., reward_exp=None):
+    if reward_exp is None:
+        reward_exp = float(cfg.reward_exp)
+
+    if cfg.anneal == "linear":
+        process_ratio = max(0., min(1., process_ratio)) # from 0 to 1
+        reward_exp = reward_exp * process_ratio +\
+                     float(cfg.reward_exp_init) * (1 - process_ratio)
+    elif cfg.anneal == "none":
+        pass
+    else:
+        raise NotImplementedError
+
+    # (R/T)^beta -> (log R - log T) * beta
+    def logr_scaler(sol_size, gbatch=None):
+        logr = sol_size
+        return logr * reward_exp
+    return logr_scaler
+
+def refine_cfg(cfg):
+    with open_dict(cfg):
+        cfg.device = cfg.d
+        cfg.work_directory = os.getcwd()
+
+        if cfg.task in ["mis", "maxindset", "maxindependentset",]:
+            cfg.task = "MaxIndependentSet"
+            cfg.wandb_project_name = "MIS"
+        elif cfg.task in ["mds", "mindomset", "mindominateset",]:
+            cfg.task = "MinDominateSet"
+            cfg.wandb_project_name = "MDS"
+        elif cfg.task in ["mc", "maxclique",]:
+            cfg.task = "MaxClique"
+            cfg.wandb_project_name = "MaxClique"
+        elif cfg.task in ["mcut", "maxcut",]:
+            cfg.task = "MaxCut"
+            cfg.wandb_project_name = "MaxCut"
+        else:
+            raise NotImplementedError
+
+        # architecture
+        assert cfg.arch in ["gin"]
+
+        # log reward shape
+        cfg.reward_exp = cfg.rexp
+        cfg.reward_exp_init = cfg.rexpit
+        if cfg.anneal in ["lin"]:
+            cfg.anneal = "linear"
+
+        # training
+        cfg.batch_size = cfg.bs
+        cfg.batch_size_interact = cfg.bsit
+        cfg.leaf_coef = cfg.lc
+        cfg.same_graph_across_batch = cfg.sameg
+
+        # data
+        cfg.test_batch_size = cfg.tbs
+        if "rb" in cfg.input:
+            cfg.data_type = cfg.input.upper()
+        elif "ba" in cfg.input:
+            cfg.data_type = cfg.input.upper()
+        else:
+            raise NotImplementedError
+
+    del cfg.d, cfg.rexp, cfg.rexpit, cfg.bs, cfg.bsit, cfg.lc, cfg.sameg, cfg.tbs
+    return cfg
+
+@torch.no_grad()
+def rollout(gbatch, cfg, alg):
+    env = get_mdp_class(cfg.task)(gbatch, cfg)
+    state = env.state
+
+    ##### sample traj
+    reward_exp_eval = None
+    traj_s, traj_r, traj_a, traj_d = [], [], [], []
+    while not all(env.done):
+        action = alg.sample(gbatch, state, env.done, rand_prob=cfg.randp, reward_exp=reward_exp_eval)
+
+        traj_s.append(state)
+        traj_r.append(env.get_log_reward())
+        traj_a.append(action)
+        traj_d.append(env.done)
+        state = env.step(action)
+
+    ##### save last state
+    traj_s.append(state)
+    traj_r.append(env.get_log_reward())
+    traj_d.append(env.done)
+    assert len(traj_s) == len(traj_a) + 1 == len(traj_r) == len(traj_d)
+
+    traj_s = torch.stack(traj_s, dim=1) # (sum of #node per graph in batch, max_traj_len)
+    traj_r = torch.stack(traj_r, dim=1) # (batch_size, max_traj_len)
+    traj_a = torch.stack(traj_a, dim=1) # (batch_size, max_traj_len-1)
+    """
+    traj_a is tensor like 
+    [ 4, 30, 86, 95, 96, 29, -1, -1],
+    [47, 60, 41, 11, 55, 64, 80, -1],
+    [26, 38, 13,  5,  9, -1, -1, -1]
+    """
+    traj_d = torch.stack(traj_d, dim=1) # (batch_size, max_traj_len)
+    """
+    traj_d is tensor like 
+    [False, False, False, False, False, False,  True,  True,  True],
+    [False, False, False, False, False, False, False,  True,  True],
+    [False, False, False, False, False,  True,  True,  True,  True]
+    """
+    traj_len = 1 + torch.sum(~traj_d, dim=1) # (batch_size, )
+
+    ##### graph, state, action, done, reward, trajectory length
+    batch = gbatch.cpu(), traj_s.cpu(), traj_a.cpu(), traj_d.cpu(), traj_r.cpu(), traj_len.cpu()
+    return batch, env.batch_metric(state)
+
+
+@hydra.main(config_path="configs", config_name="main") # for hydra-core==1.1.0
+# @hydra.main(version_base=None, config_path="configs", config_name="main") # for newer hydra
+def sample(cfg: DictConfig):
+
+    cfg = refine_cfg(cfg)
+    device = torch.device(f"cuda:{cfg.device:d}" if torch.cuda.is_available() and cfg.device>=0 else "cpu")
+    print(f"Device: {device}")
+    
+    ### Allow usage of reference algorithms
+    assert (cfg.ref_alg != "") and (cfg.fixed_steps != 0)
+    ref_alg = get_reference_alg(cfg)
+    
+    alg, _ = get_saved_alg_buffer(cfg, device)
+    alg_name = cfg.alg_load_path.split(".")[0].split("/")[-1]
+    
+    seed_torch(cfg.seed)
+    print(str(cfg))
+    print(f"Work directory: {os.getcwd()}")
+    print(f"Algorithm: {alg.__class__.__name__}")
+
+    _, test_loader = get_data_loaders(cfg)
+    
+    @torch.no_grad()
+    def evaluate(ep, logr_scaler):
+
+        if not os.path.exists(f'/content/GFlowNet-CombOpt/states/{alg_name}/{ep}'):
+            os.makedirs(f'/content/GFlowNet-CombOpt/states/{alg_name}/{ep}')
+        
+        torch.cuda.empty_cache()
+        num_repeat = 1
+        mis_ls, mis_top20_ls = [], []
+        logr_ls = []
+        pbar = tqdm(enumerate(test_loader))        
+                
+        for batch_idx, gbatch in pbar:
+
+            gbatch = gbatch.to(device)
+            gbatch_rep = dgl.batch([gbatch] * num_repeat)
+
+            env = get_mdp_class(cfg.task)(gbatch_rep, cfg)
+            state = env.state
+            actions = []
+            states = []
+
+            state_per_graph = torch.split(state, env.numnode_per_graph, dim=0)
+            state_per_graph = [state.cpu().numpy() for state in state_per_graph]
+            states.append(state_per_graph)
+
+            step = 0
+            while not all(env.done):
+
+                if step < cfg.fixed_steps:
+                    alg_out = ref_alg.sample(gbatch_rep, state, env.done, rand_prob=0.)
+                else:
+                    alg_out = alg.sample(gbatch_rep, state, env.done, rand_prob=0.)
+                
+                if len(alg_out) == 2:
+                    action, _ = alg_out
+                else:
+                    action = alg_out
+
+                actions.append(action.cpu().numpy())
+                state = env.step(action)
+                state_per_graph = torch.split(state, env.numnode_per_graph, dim=0)
+                state_per_graph = [state.cpu().numpy() for state in state_per_graph]
+                states.append(state_per_graph)
+
+            for graph in range(len(state_per_graph)):
+                graph_data = np.array([[]])
+                for i in range(len(states)):
+                    if i==0:
+                        graph_data = np.array([states[i][graph]])
+                    else:
+                        graph_data = np.append(graph_data, np.array([states[i][graph]]), axis=0)
+                np.save(f'/content/GFlowNet-CombOpt/states/{alg_name}/{ep}/{batch_idx}_{graph}', graph_data)
+            
+            logr_rep = logr_scaler(env.get_log_reward())
+            logr_ls += logr_rep.tolist()
+            curr_mis_rep = torch.tensor(env.batch_metric(state))
+            curr_mis_rep = rearrange(curr_mis_rep, "(rep b) -> b rep", rep=num_repeat).float()
+            mis_ls += curr_mis_rep.mean(dim=1).tolist()
+            mis_top20_ls += curr_mis_rep.max(dim=1)[0].tolist()
+            pbar.set_postfix({"Metric": f"{np.mean(mis_ls):.2f}+-{np.std(mis_ls):.2f}"})
+
+        print(f"Test Epoch{ep:2d}: "
+              f"Metric={np.mean(mis_ls):.2f}+-{np.std(mis_ls):.2f}, "
+              f"top20={np.mean(mis_top20_ls):.2f}, "
+              f"LogR scaled={np.mean(logr_ls):.2e}+-{np.std(logr_ls):.2e}")
+
+        return states, actions
+
+    ##### sample
+    for ep in range(cfg.epochs):
+        process_ratio = 1
+        reward_exp = None
+        logr_scaler = get_logr_scaler(cfg, process_ratio=process_ratio, reward_exp=reward_exp)
+        states, actions = evaluate(ep, logr_scaler)
+
+if __name__ == "__main__":
+    sample()
